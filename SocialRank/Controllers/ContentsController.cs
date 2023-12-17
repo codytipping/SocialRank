90a5b6755555f68ad0868a228b9d4cfa6f33b9f0
Cody Tipping <codyctipping@gmail.com>
2023-12-17 15:22:29 -0500

    Created CRUD Operations for Contents (VC).

---
+++ SocialRank/Controllers/ContentsController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialRank.Data;
+using SocialRank.Models;
+
+namespace SocialRank.Controllers
+{
+    public class ContentsController : Controller
+    {
+        private readonly SocialRankContext _context;
+
+        public ContentsController(SocialRankContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+              return _context.Contents != null ? 
+                          View(await _context.Contents.ToListAsync()) :
+                          Problem("Entity set 'SocialRankContext.Contents'  is null.");
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Rank")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(content);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+            return View(content);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Rank")] Content content)
+        {
+            if (id != content.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(content);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentExists(content.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Contents == null)
+            {
+                return Problem("Entity set 'SocialRankContext.Contents'  is null.");
+            }
+            var content = await _context.Contents.FindAsync(id);
+            if (content != null)
+            {
+                _context.Contents.Remove(content);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentExists(string id)
+        {
+          return (_context.Contents?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
